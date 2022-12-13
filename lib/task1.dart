95136542c7b82a9cdfb1038dc74c8428ca5f6502
Alex Ushkov <alexushkov82@gmail.com>
2022-12-13 12:30:44 +0300

    Task 1 Done.

--- lib/task1.dart
+++ lib/task1.dart
@@ -1,3 +1,15 @@
+import 'dart:io';
+
 void main() {
-  print('task1');
+  print("Put your string here?");
+  String string = stdin.readLineSync()!;
+  int length = string.length;
+  bool isPalindrom = true;
+  for (int i = 0; i < length/2; i++) {
+    if (string[i] != string[length-i-1]) {
+      isPalindrom = false;
+      break;
+    }
+  }
+  print ('Palindrom: $isPalindrom');
 }
