ac010b5bf721cb6c9bc57f9ec5baaed8eb6e3188
Alex Ushkov <alexushkov82@gmail.com>
2022-12-14 11:59:48 +0300

    Task 9 Done.

---
+++ lib/task9.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+import 'dart:core';
+
+void main() {
+  print('Put your number here:');
+  String str = stdin.readLineSync()!;
+  RegExp ccn = RegExp(r'^[0-9]{16}$');
+  //RegExp ccn = RegExp(r'^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$');
+  print(ccn.hasMatch(str));
+}
